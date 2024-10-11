a8e91ee37341c57d43bd4117b25c3105425d0a6d
Raymond Huang <raymondhuang@umass.edu>
2024-10-11 12:48:56 -0400

    calculateReciprocal

--- calculator.js
+++ calculator.js
@@ -14,11 +14,12 @@ function calculateSquare(value) {
 }
 
 // TODO: FIX reciprocal function
-function calculateReciprocal() {
-    // Hint: Reciprocal is 1/x
-    // return the square root of the value
-    return 1* value;
-
+function calculateReciprocal(value) {
+    if(number === 0){
+        return undefined;
+    }else{
+        return 1/value;
+    };
 }
 
 // TODO: Implement natural log function
